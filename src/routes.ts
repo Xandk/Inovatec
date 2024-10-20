7c6b39f0cfaa85186b7684b355c5d028766e3097
XandK <borgesdearaujofilhoalexandre@gmail.com>
2024-10-20 18:56:25 -0300

    Terceiro commit

--- src/routes.ts
+++ src/routes.ts
@@ -10,7 +10,19 @@ import { CreateNutritionController } from './controllers/CreateNutritionControll
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
 
     fastify.get("/teste", (request: FastifyRequest, reply: FastifyReply) => {
-        console.log("ROTA CHAMADA!")
+        let responseText = "```json\n{\n  \"nome\": \"Alexandre\",\n  \"sexo\": \"Masculino\",\n  \"idade\": 20,\n  \"altura\": 1.90,\n  \"peso\": 90,\n  \"objetivo\": \"Hipertrofia\",\n  \"refeicoes\": [\n    {\n      \"horario\": \"08:00\",\n      \"nome\": \"Cafe da Manha\",\n      \"alimentos\": [\n        \"2 fatias de pao integral\",\n        \"2 ovos mexidos com queijo cottage\",\n        \"1 banana\",\n        \"1 copo de leite desnatado\"\n      ]\n    },\n    {\n      \"horario\": \"10:00\",\n      \"nome\": \"Lanche da Manha\",\n        \"alimentos\": [\n        \"1 iogurte grego com granola\"\n      ]\n    },\n    {\n      \"horario\": \"13:00\",\n      \"nome\": \"Almoco\",\n      \"alimentos\": [\n        \"150g de frango grelhado\",\n        \"100g de batata doce\",\n        \"100g de arroz integral\",\n        \"Salada verde com azeite de oliva\"\n      ]\n    },\n    {\n      \"horario\": \"15:30\",\n      \"nome\": \"Lanche da Tarde\",\n      \"alimentos\": [\n        \"1 scoop de whey protein\",\n        \"1 banana\"\n      ]\n    },\n    {\n      \"horario\": \"19:00\",\n      \"nome\": \"Jantar\",\n      \"alimentos\": [\n        \"150g de peixe grelhado\",\n        \"100g de brócolis\",\n        \"100g de batata doce\"\n      ]\n    },\n    {\n      \"horario\": \"21:00\",\n      \"nome\": \"Lanche da Noite\",\n      \"alimentos\": [\n        \"1 scoop de caseína\"\n      ]\n    }\n  ],\n  \"suplementos\": [\n    \"Whey Protein\",\n    \"Creatina\",\n    \"BCAA\",\n    \"Glutamina\"\n  ]\n}\n```"
+
+        try {
+            
+            let jsonString = responseText.replace(/```\w*\n/g, '').replace(/\n```/g, '').trim();
+
+            let jsonObject = JSON.parse(jsonString)
+
+            return reply.send( {data: jsonObject} );
+
+        } catch (err) {
+            console.log(err)
+        }
 
         reply.send({ ok: true})
 
